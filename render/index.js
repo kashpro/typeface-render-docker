4399983d9a46e8bf8effdab362753d4474f8bb71
kashpro <kash083@gmail.com>
2024-02-16 16:31:11 +0800

    testing buffers

--- render/index.js
+++ render/index.js
@@ -1,10 +1,19 @@
-console.log("hello world");
+const fs = require('fs');
+const gm = require('gm');
 
-var fs = require('fs')
-  , gm = require('gm');
+const picture = fs.readFileSync('./1.webp');
 
-gm('./1.webp')
+
+gm(picture)
 .rotate('green', 45)
+.noise('laplacian')
 .write('./crazy.jpg', function (err) {
   if (!err) console.log('crazytown has arrived');
+})
+
+gm(picture)
+.rotate('red', 0)
+.noise('laplacian')
+.write('./crazy2.jpg', function (err) {
+  if (!err) console.log('crazytown2 has arrived');
 })
